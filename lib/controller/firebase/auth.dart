e1cd0199fbec25d94ee80cfa3722e618bbfa58c6
Larsanov <ismail.larsanov@epitech.eu>
2020-05-04 03:00:22 +0200

    firebase auth

--- lib/controller/firebase/auth.dart
+++ lib/controller/firebase/auth.dart
@@ -1,3 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
 class Auth {
-  
+  static Widget handleAuth(Widget index, Widget mainAppController) {
+    return StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context, snapshot) {
+        return (!snapshot.hasData) ? index : mainAppController;
+      },
+    );
+  }
 }
