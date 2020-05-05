6acf43eb9187564d7eb8ca36ad3c6e8c69463773
Larsanov <ismail.larsanov@epitech.eu>
2020-05-05 19:19:02 +0200

    compress image

--- lib/controller/system/image.dart
+++ lib/controller/system/image.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:material_neworld/controller/firebase/cloud.dart';
 
@@ -6,8 +7,14 @@ class Image {
   static gallery({String uploadTable, String me, Map<String, dynamic> map}) async {
     final _image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
+    var compress = await FlutterImageCompress.compressWithFile(
+      _image.absolute.path,
+      minWidth: 500,
+      minHeight: 500,
+      quality: 85,
+    );
     Cloud.uploadImage(
-      image: _image,
+      image: compress,
       me: me,
       uploadTable: uploadTable,
       map: map,
@@ -17,8 +24,14 @@ class Image {
   static camera({String uploadTable, String me, Map<String, dynamic> map}) async {
     final _image = await ImagePicker.pickImage(source: ImageSource.camera);
 
+    var compress = await FlutterImageCompress.compressWithFile(
+      _image.absolute.path,
+      minWidth: 500,
+      minHeight: 500,
+      quality: 85,
+    );
     Cloud.uploadImage(
-      image: _image,
+      image: compress,
       me: me,
       uploadTable: uploadTable,
       map: map,
